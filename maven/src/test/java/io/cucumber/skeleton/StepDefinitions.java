cc4e805a130e8e54ff26ab394d95bc8cc991efca
MalfiRG <malfiraggraclan@gmail.com>
2023-05-03 00:39:43 +0200

    The cucumber framework after passing The Tutorial: https://cucumber.io/docs/guides/10-minute-tutorial/?lang=java

--- maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,46 @@
 package io.cucumber.skeleton;
-
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class IsItFriday {
+    // alternative code:
+//    static Object isItFriday(String today) {
+//                if (today == "Friday") {
+//                    return "TGIF";
+//                }
+//                else if (today == "Sunday") {
+//                    return "Nope";
+//                else {
+//                    return "Nope";
+//                }
+         static String isItFriday(String today) {
+    return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
 public class StepDefinitions {
-    @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
-        belly.eat(cukes);
+    private String today;
+    private String actualAnswer;
+
+    //the actual code for this step is:
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+
+    }
+    /*Deprecated code
+    *@Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    } */
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = (String) IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 }
